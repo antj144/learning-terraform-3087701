2e8bc7f94e16b95adaf4f40cf15e2793c2f30e06
Anthony Jansen <antj669@gmail.com>
2025-07-22 11:31:11 +1000

    autoscaling

--- main.tf
+++ main.tf
@@ -33,18 +33,22 @@ module "blog_vpc" {
   }
 }
 
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "9.0.1"
+  # insert the 1 required variable here
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
+  instance_type      = var.instance_type
 }
 
 module "blog_alb" {
