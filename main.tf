bd5f8ab7cccc706fae2f151eefaecd99ccc3b95a
Anthony Jansen <antj669@gmail.com>
2025-07-22 11:41:32 +1000

    fixes

--- main.tf
+++ main.tf
@@ -33,22 +33,19 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
+module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
-  # insert the 1 required variable here
+  version = "6.5.2"
 
   name = "blog"
-  min_size = 1
-  max_size = 2
 
+  min_size            = var.asg_min
+  max_size            = var.asg_max
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns[0]
-
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
-  instance_type      = var.instance_type
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -85,18 +82,14 @@ module "blog_alb" {
   }
 }
 
-
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
-
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules        = ["http-80-tcp","https-443-tcp"]
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules         = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
